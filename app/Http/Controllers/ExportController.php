a9814458195a53e05ef2711ba0cb98e9afc8dab7
Aurapresty1105 <aurapresty11@gmail.com>
2025-03-20 04:45:53 +0700

    fix: validation for export pdf

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
 use App\Models\Presensi;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class ExportController extends Controller
 {
@@ -12,6 +13,13 @@ class ExportController extends Controller
     {
         // Ambil seluruh data presensi dengan relasi siswa
         $presensi = Presensi::with(['siswa.user', 'siswa.kelas'])->get();
+
+        // Periksa apakah data presensi kosong
+        if ($presensi->isEmpty()) {
+            // Berikan alert atau redirect dengan pesan error
+            Alert::error('Peringatan', 'Data presensi masih kosong.');
+            return back();
+        }
 
         // Kelompokkan data berdasarkan siswa dan hitung total kehadiran
         $data = $presensi->groupBy('id_siswa')->map(function ($items) {
@@ -28,7 +36,7 @@ class ExportController extends Controller
 
         // Load view untuk PDF
         $pdf = Pdf::loadView('menu.exports.presensi_pdf', ['data' => $data])
-               ->setPaper('a4', 'landscape');
+            ->setPaper('a4', 'landscape');
 
         // Unduh file PDF
         return $pdf->download('data-kehadiran.pdf');
